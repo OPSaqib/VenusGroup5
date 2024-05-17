89f21b46aa6269a9f011bb3fe1e5bfc46344eade
MIC <168171020+BongSpyder@users.noreply.github.com>
2024-05-17 11:05:23 +0200

    Update Communication.py

--- Communication/Communication.py
+++ Communication/Communication.py
@@ -8,11 +8,11 @@ import sys
 broker_address = "mqtt.ics.ele.tue.nl"
 topic_publish = "/pynqbridge/5/send"
 topic_subscribe = "/pynqbridge/5/recv"
-username = "Student09"  #use Student09 or Student10
-password = "Ok1Kasie"   #use Ok1Kasie for Student09 of Ea6leof9 for Student10
+username = "Student09"  # Use Student09 or Student10
+password = "Ok1Kasie"   # Use Ok1Kasie for Student09 of Ea6leof9 for Student10
 
 # Select mode:
-mode_0_or_1 = 1         #1 for testing, 0 for running
+mode_0_or_1 = 1         # 1 for testing, 0 for running
 if mode_0_or_1 == 1:
     topic_subscribe = topic_publish
 
@@ -29,7 +29,7 @@ def on_message(client, userdata, message):
     print("Received message: " + str(message.payload.decode("utf-8")) + "| on topic " + message.topic)
     
 # Setup MQTT client and callbacks
-client = mqtt.Client("Example", clean_session=True)
+client = mqtt.Client("9", clean_session=True)                              # Use 9 for Student9 or 10 for Student10
 client.on_connect = on_connect
 client.on_message = on_message
 
@@ -38,21 +38,21 @@ client.username_pw_set(username, password)
 
 # start the infinite loop
 try:
-    client.connect(broker_address, port=1883)                               #connect to the broker
+    client.connect(broker_address, port=1883)                               # Connect to the broker
     client.loop_start()
     msg_count = 0
-    while True:                                                             #publish messages infinitelly
-        if mode_0_or_1 == 1:                                                #testting
+    while True:                                                             # Publish messages infinitelly
+        if mode_0_or_1 == 1:                                                # Testting
             message = f"l_speed {msg_count}"
-        else:                                                               #actual message
+        else:                                                               # Actual message
             message = f"r_speed {msg_count}"
-            print("Send message: %s" %message)                              #check what message was sent
+            print("Send message: %s" %message)                              # Check what message was sent
         client.publish(topic_publish, message)
         msg_count += 1
-        time.sleep(3)                                                       #wait b4 next message
+        time.sleep(3)                                                       # Wait b4 next message
 except Exception as error:
     print(f"Failed to connect to MQTT broker at {broker_address}: {error}")
-except KeyboardInterrupt:                                                   #exit when CTRL+C pressed
+except KeyboardInterrupt:                                                   # Exit when CTRL+C pressed
     print("Exiting...")
 finally:
     client.loop_stop
