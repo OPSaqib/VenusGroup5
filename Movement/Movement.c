587130368d6c2ae4647d8c4d305a3fe450269ed5
OPSaqib <131691880+OPSaqib@users.noreply.github.com>
2024-05-22 17:17:52 +0200

    Update Movement.c

--- Movement/Movement.c
+++ Movement/Movement.c
@@ -6,6 +6,29 @@
 #include <stdint.h>
 #include <stepper.h>
 
+//NOTE: stepper_set_speed(left, right) and stepper_steps(left, right) ie left tire is left tuple and 
+//right tire is the right tuple
+
+//turn the robot right by: right speed X, steps Y, then left steps Y/2, speed X*2 (so goes 1/2 as fast)
+void right() {
+  stepper_set_speed(3072, 6144);
+  stepper_steps(-400, -1600); //CAN BE MODIFIED
+  while (!stepper_steps_done()) {}; //Wait for stepper steps to finish
+}
+
+//turn the robot left by: left speed X, steps Y, then right steps Y/2, speed X*2 (so goes 1/2 as fast)
+void left() {
+  stepper_set_speed(6144, 3072);
+  stepper_steps(-1600, -400); //CAN BE MODIFIED
+  while (!stepper_steps_done()) {}; //Wait for stepper steps to finish
+}
+
+void forwards() {
+  stepper_set_speed(6144, 6144);
+  stepper_steps(-200, -200); //CAN BE MODIFIED
+  while (!stepper_steps_done()) {}; //Wait for stepper steps to finish
+}
+
 int main(void) {
   pynq_init();
   // Initialize the stepper driver.
@@ -14,38 +37,22 @@ int main(void) {
   stepper_enable();
   
   //RUN THE ROBOT
-  forward();
-  right();
-  left();
+  //right();
+  //sleep_msec(100);
+  //forwards();
+  //left();
   //RUN THE ROBOT
+
+  //right();
+
+  //left();
+  //sleep_msec(100);
+  forwards();
+  sleep_msec(5000);
+  left();
 
   stepper_destroy();
   pynq_destroy();
 
   return EXIT_SUCCESS;
-}
-
-//move the robot forwards
-void forward() {
-    stepper_set_speed(6144, 6144);
-    stepper_steps(200, 200); //CAN BE MODIFIED
-    while (!stepper_steps_done()) {}; //Wait for stepper steps to finish
-}
-
-//turn the robot right
-void right() {
-    stepper_set_speed(3072, 6144);
-    stepper_steps(100, 200); //CAN BE MODIFIED
-    while (!stepper_steps_done()) {}; //Wait for stepper steps to finish
-}
-
-//turn the robot left 
-void left() {
-    stepper_set_speed(6144, 3072);
-    stepper_steps(200, 100); //CAN BE MODIFIED
-    while (!stepper_steps_done()) {}; //Wait for stepper steps to finish
-}
-
-void fullrotation() {
-    //this situation shouldn't ever happen as the algorithm should be as such that the robot never encounters a deadlock situation
 }
