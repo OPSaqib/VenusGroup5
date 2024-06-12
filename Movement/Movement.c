c1618d74eee10d76e0839deceebb46af4f670605
OPSaqib <131691880+OPSaqib@users.noreply.github.com>
2024-06-12 14:45:58 +0200

    Update Movement.c

--- Movement/Movement.c
+++ Movement/Movement.c
@@ -20,72 +20,58 @@ void disable_stepper() {
 
 //turn the robot right by: right speed X, steps Y, then left steps Y/2, speed X*2 (so goes 1/2 as fast)
 void right() {
-  enable_stepper();
-  stepper_set_speed(6144, 3072);
-  stepper_steps(-1400, -200); //CAN BE MODIFIED
+  //enable_stepper();
+  stepper_set_speed(-6144, -3072);
+  stepper_steps(-1450, -200); //CAN BE MODIFIED
   while (!stepper_steps_done()) {}; //Wait for stepper steps to finish
-  disable_stepper();
+  //disable_stepper();
 }
 
 //TODO:
 //turn the robot left by: left speed X, steps Y, then right steps Y/2, speed X*2 (so goes 1/2 as fast)
 void left() {
-  enable_stepper();
-  stepper_set_speed(3072, 6144);
-  stepper_steps(-200, -1350); //CAN BE MODIFIED
+  //enable_stepper();
+  stepper_set_speed(-3072, -6144);
+  stepper_steps(-200, -1400); //CAN BE MODIFIED
   while (!stepper_steps_done()) {}; //Wait for stepper steps to finish
-  disable_stepper();
-}
-
-//NOT REQUIRED:
-void make_left_slightly() {
-  enable_stepper();
-  stepper_set_speed(3072, 6144);
-  stepper_steps(-200, -400); //CAN BE MODIFIED
-  while (!stepper_steps_done()) {}; //Wait for stepper steps to finish
-  disable_stepper();
-}
-
-//NOT REQUIRED:
-void make_right_slightly() {
-  enable_stepper();
-  stepper_set_speed(6144, 3072);
-  stepper_steps(-150, -50); //CAN BE MODIFIED
-  while (!stepper_steps_done()) {}; //Wait for stepper steps to finish
-  disable_stepper();
+  //disable_stepper();
 }
 
 void forwards() {
-  enable_stepper();
-  stepper_set_speed(3072, 3072);
+  //enable_stepper();
+  stepper_set_speed(-3072, -3072);
   stepper_steps(-200, -200); //CAN BE MODIFIED
   while (!stepper_steps_done()) {}; //Wait for stepper steps to finish
-  disable_stepper();
+  //disable_stepper();
+}
+
+void backwards() {
+  //enable_stepper();
+  stepper_set_speed(3072, 3072);
+  stepper_steps(400, 400); //CAN BE MODIFIED
+  while (!stepper_steps_done()) {}; //Wait for stepper steps to finish
+  //disable_stepper();
 }
 
 int main(void) {
   pynq_init();
   // Initialize the stepper driver.
-
-  // Now do the movements required
+  enable_stepper();
   forwards();
-  sleep_msec(2000);
+  sleep_msec(20);
   forwards();
-  sleep_msec(2000);
+  sleep_msec(20);
+  forwards();
+  sleep_msec(20);
+  forwards();
+  sleep_msec(20);
+  forwards();
+  sleep_msec(20);
   left();
-  //sleep_msec(2000);
-  //make_right_slightly();
-  sleep_msec(2000);
+  sleep_msec(20);
   forwards();
-  sleep_msec(2000);
+  sleep_msec(20);
   forwards();
-  sleep_msec(2000);
-  forwards();
-  //sleep_msec(2000);
-  //left();
-  //sleep_msec(2000);
-  //forwards();
-  pynq_destroy();
-
+  disable_stepper();
   return EXIT_SUCCESS;
 }
